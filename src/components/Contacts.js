6112ff4006ea492c9629e94042e035a63155d02c
TsikaBullRat <tsika1996190514@gmail.com>
2021-07-15 11:15:09 +0200

    Fixes

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -41,12 +41,12 @@ export const Contacts = () => {
                     <div className="col-lg-6  col-sm-12">
 
                         <div className="mt-5">
-                            <form onSubmit={sendEmail}>
+                            <form onSubmit={sendEmail} className="form">
                                 <input type="email" placeholder="Email" className="form-control w-50" name="email" aria-describedby="emailHelp" />
                                 <input type="text" placeholder="Subject" className="form-control w-50 mt-3" name="subject" aria-describedby="emailHelp" /> <br></br>
-                                <textarea className="form control w-50 mt-3" name="matter" rows="3" placeholder="Message"></textarea> <br></br>
-                                <div className="d-flex mt-1">
-                                    <button name="submit" type="submit" className="btn ml-2 mr-2">Send</button> <br></br>
+                                <textarea className="form control w-50 mt-3 ml-0" name="matter" rows="3" placeholder="Message"></textarea> <br></br>
+                                <div className="d-flex">
+                                    <button name="submit" type="submit" className="btn ml-0 mr-2">Send</button> <br></br>
                                     <button name="reset" type="reset" className="btn ml-2 mr-2">Reset</button> <br></br>
                                 </div>
                             </form>
