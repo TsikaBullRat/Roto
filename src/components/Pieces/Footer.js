7e252f2a2caaf2ac8a15e7561a8ed25d04db4beb
TsikaBullRat <tsika1996190514@gmail.com>
2021-07-15 16:01:52 +0200

    New changes

--- src/components/Pieces/Footer.js
+++ src/components/Pieces/Footer.js
@@ -8,19 +8,19 @@ function Footer() {
             <div className="d-flex mt-2 col-lg-12 col-md-12 col-sm-12">
 
                 <div className="text-center col-lg-4 col-md-6 col-sm-12">
-                    <i className="bi bi-envelope" style={{ color: '#fff', fontSize: 25 }}></i><h2>Phone</h2>
+                    <i className="bi bi-envelope" style={{ color: '#000', fontSize: 25 }}></i><h2>Phone</h2>
                     <h3><b>0630253221</b></h3>
                     <h3>Head Office</h3>
                 </div>
 
                 <div className="text-center col-lg-4 col-md-6 col-sm-12">
-                    <i className="bi bi-telephone" style={{ color: '#fff', fontSize: 25 }}></i><h2>Email</h2>
+                    <i className="bi bi-telephone" style={{ color: '#000', fontSize: 25 }}></i><h2>Email</h2>
                     <h3><b>mphomatthews597@gmail.com</b></h3>
                     <h3>Head Office</h3>
                 </div>
 
                 <div class="text-center col-lg-4 col-md-12 col-sm-12">
-                    <i className="bi bi-geo-alt-fill" style={{ color: '#fff', fontSize: 25 }}></i><h2>Address:</h2>
+                    <i className="bi bi-geo-alt-fill" style={{ color: '#000', fontSize: 25 }}></i><h2>Address:</h2>
                     <h3><b>Barkley West</b></h3>
                     <h3>Kimberley</h3>
                 </div>
